5f232f4bca252825290afbc917586e800ecd9508
David Kingsley <31980518+dkingsjr@users.noreply.github.com>
2023-01-15 21:02:53 -0600

    Update PJS_RandWinGen.py
    Replaced wrong code with correct code.

--- PJS_RandWinGen.py
+++ PJS_RandWinGen.py
@@ -1,96 +1,76 @@
 from tkinter import *
-from random import randint
 import random
- 
+
+
+def add_participant():
+    participant = entry.get()
+    participants.append(participant)
+    entry.delete(0, END)
+
+
+def delete_all():
+    participants.clear()
+
+
+def select_winner():
+    if participants:
+        winner = random.choice(participants)
+        label_winner.config(text=f"{winner}", fg='orange', font="bold 50")
+        participants.remove(winner)
+    else:
+        label_winner.config(text="No participants", fg='red', font="bold 50")
+
+
+def exit_program():
+    root.destroy()
+
+
 root = Tk()
-root.title('PinPassGen 1.5')
-root.geometry("500x300")
-root.configure(bg="lightgrey")
-app_width = 500
-app_height = 300
+root.title("PJS Scholarship Award")
+app_width = 573
+app_height = 770
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 x = (screen_width / 2) - (app_width / 2)
 y = (screen_height / 2) - (app_height / 2)
 root.geometry(f'{app_width}x{app_height}+{int(x)}+{int(y)}')
+root.configure(bg='#404040')
 
-TYPE = IntVar()
-result = "12345"
- 
- 
-# Create Password
-def make_Password(length):
-    my_password = "".join([chr(randint(33, 126)) for _ in range(length)])
-    return my_password
- 
-#Create a PIN
-def make_Pin(length):
-    my_pin = "".join([chr(randint(48,57)) for _ in range(length)])
-    return my_pin
- 
-#Copy to Clipboard 
-def clipper():
-    root.clipboard_clear()
-    root.clipboard_append(result)
-    root.update()
- 
-def passpin():
-    global result
-    pw_result.delete(0, END)                # Clear result field
-    len_str = my_entry.get()                # Get length as a string
-    if len_str.isdigit():                   # Check for numeric length
-        res_length = int(len_str)           # get required length of Pin or Password
-        if TYPE.get() == 1:                 # Compute a PIN
-            if res_length > 15:
-                result = "Max Pin length is 15"
-            else:
-                result = make_Pin(res_length)       # Call make_Pin(), telling it the length required
-        else:                                   # Generate a Password
-            result = make_Password(res_length)  # Call make_Password(), telling it the length required
-    else:
-        result = "Invalid length"
-    pw_result.insert(0, result)             # Display PIN or Password in output field
- 
- 
-# Create a frame for our input box
-lf = LabelFrame(root, text="How Many Characters?", bg="lightgrey", width=10)
-lf.grid(row=0, columnspan=3, padx=50, pady=5)
- 
-# Create a frame for our Radio Buttons
-radf = LabelFrame(root, text="Choose Security Type", bg="lightgrey")
-radf.grid(row=1, column=0, padx=50, pady=5)
- 
-# create a frame for our buttons
-my_frame = LabelFrame(root, text="Generate/Copy", bg="lightgrey")
-my_frame.grid(row=2, column=0, padx=50, pady=5)
- 
-# create a frame for our output box
-my_output = LabelFrame(root, text="Your New Pin/Password", bg="lightgrey")
-my_output.grid(row=3, column=0, padx=50, pady=5)
- 
-# Create entry box to designate number of characters
-my_entry = Entry(lf, font=("helvetica", 24), width=7, justify="center")
-my_entry.grid(row=0, column=0, padx=5, pady=2)
- 
-# create result box for our returned password
-pw_result = Entry(my_output, text='', font=("helvetica", 24), bd=0, bg="lightgrey")
-pw_result.grid(row=0, columnspan=3)
- 
-# create our buttons
-# my_button = Button(my_frame, text="Generate Pin/Pass", command=Password, bg="lightgrey")
-my_button = Button(my_frame, text="Generate Pin/Pass", command=passpin, bg="lightgrey")
-my_button.grid(row=1, column=0, padx=10)
- 
-clip_button = Button(my_frame, text="Copy To Clipboard", command=clipper, bg="lightgrey")
-clip_button.grid(row=1, column=1)
- 
-# create radio buttons
-Radiobutton(radf, text="Pin", bg="lightgrey", padx=20, variable=TYPE, value=1).pack(anchor=W)
-Radiobutton(radf, text="Password", bg="lightgrey", padx=20, variable=TYPE, value=2).pack(anchor=W)
 
-credlabel = Label(root, text="Programmed by David J. Kingsley Jr. with aid from Mike K.") 
- 
-# function
-Pin = "1234567890"
- 
+
+participants = []
+
+main_frame = Frame(root, bg='#404040', relief=SUNKEN, bd=2)
+main_frame.grid(row=1, columnspan=3, padx=30, pady=30)
+
+button_frame = Frame(main_frame, bg='#404040', relief=SUNKEN, bd=1)
+button_frame.grid(row=4, columnspan=3, padx=10, pady=10)
+
+label_title = Label(root, text="Pressure Junkies Scuba\nScholarship Winner", bg='#404040', fg='#D01110', font="bold 35")
+label_title.grid(row=0, columnspan=3, pady=20)
+
+win_label = Label(main_frame, bg='#404040', fg='#EEEDE7', text="Winner:", font="bold 36")
+win_label.grid(row=1, columnspan=3, pady=20)
+
+label_winner = Label(main_frame, text="Please enter\n participants!", bg='#404040', fg='#145DA0', font="bold 30")
+label_winner.grid(row=2, columnspan=3, pady=20)
+
+entry = Entry(button_frame, justify="center", bg='#868B8E', font="bold 30", width=15)
+entry.grid(row=0, columnspan=2, pady=10)
+
+winner_button = Button(button_frame, text="Select Winner", bg='green', fg='white', command=select_winner, width=30, height=5)
+winner_button.grid(row=1, column=0, padx=10)
+
+exit_button = Button(button_frame, text="Exit", bg='#D01110', fg='white', command=exit_program, width=30, height=5)
+exit_button.grid(row=1, column=1, padx=10, pady=10)
+
+add_button = Button(button_frame, text="Add", bg="#145DA0", fg="silver", command=add_participant, width=30, height=5)
+add_button.grid(row=2, column=0, padx=10, pady=10)
+
+delete_button = Button(button_frame, text="Delete All", bg="#145DA0", fg="silver", command=delete_all, width=30, height=5)
+delete_button.grid(row=2, column=1, padx=10)
+
+#for button in button_frame.winfo_children():
+ #   button.configure(width=30, height=5)
+
 root.mainloop()
